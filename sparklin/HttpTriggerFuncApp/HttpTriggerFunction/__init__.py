38d5a8d910b495e6f7f6a1d076fea2f95e7af2e8
Rachelle14 <145977429+Rachelle14@users.noreply.github.com>
2025-06-23 16:45:43 +0200

    Version avec className = None

--- sparklin/HttpTriggerFuncApp/HttpTriggerFunction/__init__.py
+++ sparklin/HttpTriggerFuncApp/HttpTriggerFunction/__init__.py
@@ -14,50 +14,89 @@ def uploadblob(json_in, blobname, conn_str, lin_container):
     blob = BlobClient.from_connection_string(conn_str, container_name= lin_container, blob_name=blobname)
     BlobClient
     blob.upload_blob(json_in, overwrite=True)
+    
+    try:
+        blob.upload_blob(json_in, overwrite=True)
+    except Exception as blob_err:
+        logging.error(f"Blob upload failed: {blob_err}")
+        raise
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        logging.info("http trigger function kicked off")
 
-    logging.info("http trigger function kicked off")
+        lineageContainerStr = os.environ["LINEAGE_STORAGE_CONN_STR"]
+        lineageContainer = os.environ["LINEAGE_CONTAINER"]
 
-    lineageContainerStr = os.environ["LINEAGE_STORAGE_CONN_STR"]
-    lineageContainer = os.environ["LINEAGE_CONTAINER"]
+        data = req.get_json()
+        logging.info(f"Payload reçu : {json.dumps(data)[:500]}")  # Limité à 500 caractères
 
-    data = req.get_json()
+        eventType = data.get("eventType")
+        runId = data.get("run", {}).get("runId")
+        notebookName = data.get("job", {}).get("name", "")
+        facets = data.get("run", {}).get("facets", {})
+        spark_plan = facets.get("spark.logicalPlan")
+        className = None
+        if spark_plan:
+            if "plan" in spark_plan:
+                if spark_plan["plan"]:
+                    className = spark_plan["plan"][0].get("class")
+                else:
+                    logging.warning("Plan vide dans spark.logicalPlan")
+            else:
+                logging.warning("Clé 'plan' manquante dans spark.logicalPlan")
 
-    currenttimestamp = dt.datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    eventType = data["eventType"]
-    className = data["run"]["facets"]["spark.logicalPlan"]["plan"][0]["class"]
-    runId = data["run"]["runId"]
-    notebookName = data["job"]["name"]
-    notebookName = notebookName[0 : notebookName.index('.')]
-    
-    fileName = runId + '_' + notebookName + '_' + currenttimestamp + '.json'
-    filePath = lineageContainer + '/' + fileName
+        else:
+            className = data.get("className", None)
 
-    predefined_class_list = ["org.apache.spark.sql.execution.datasources.CreateTable",
-                "org.apache.spark.sql.catalyst.plans.logical.CreateViewStatement",
-                "org.apache.spark.sql.catalyst.plans.logical.CreateTableAsSelectStatement",
-                "org.apache.spark.sql.catalyst.plans.logical.InsertIntoStatement",
-                "org.apache.spark.sql.execution.datasources.SaveIntoDataSourceCommand"]
+            logging.warning("Pas de spark.logicalPlan")
 
-    if eventType is not None and eventType == "COMPLETE" and className in predefined_class_list :
-        # upload file as json into blob storage
-        uploadblob(json.dumps(data), fileName, lineageContainerStr, lineageContainer)
-        
-        #code to add new unprocessed row for same uploaded json into azure table  
-        eventrow = event('HRSI', fileName)
-        eventrow.Status = 'Unprocessed'
-        eventrow.RetryCount = 3
-        eventrow.FilepPath = filePath
-        eventrow.isArchived = False
-        eventrow.Message = ''
+        logging.info(f"eventType={eventType}, className={className}, runId={runId}, notebookName={notebookName}")
 
-        tableStorage = tablestorage()
-        tableStorage.insertEventMetadata(eventrow.__dict__)
+        if not notebookName:
+            notebookName = "no_notebook"
 
-        return func.HttpResponse(f"Func App successfully processed http request")
+        notebookName = notebookName.split('.')[0]
 
-    else:
+        currenttimestamp = dt.datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        fileName = f"{runId}_{notebookName}_{currenttimestamp}.json"
+        filePath = f"{lineageContainer}/{fileName}"
 
-        return func.HttpResponse(f"Event Type is not COMPLETE Or ClassName Not Matched")
+        predefined_class_list = [
+            "org.apache.spark.sql.execution.datasources.CreateTable",
+            "org.apache.spark.sql.catalyst.plans.logical.CreateViewStatement",
+            "org.apache.spark.sql.catalyst.plans.logical.CreateTableAsSelectStatement",
+            "org.apache.spark.sql.catalyst.plans.logical.InsertIntoStatement",
+            "org.apache.spark.sql.execution.datasources.SaveIntoDataSourceCommand",
+            "org.apache.spark.sql.catalyst.plans.logical.MergeIntoTable"
+        ]
+
+        if eventType == "COMPLETE" and className in predefined_class_list:
+            try:
+                uploadblob(json.dumps(data), fileName, lineageContainerStr, lineageContainer)
+                logging.info(f"Blob upload OK : {filePath}")
+            except Exception as blob_err:
+                logging.error(f"Blob upload failed: {blob_err}")
+                return func.HttpResponse(f"Error uploading blob: {str(blob_err)}", status_code=500)
+
+            # code to add row in table storage
+            eventrow = event('HRSI', fileName)
+            eventrow.Status = 'Unprocessed'
+            eventrow.RetryCount = 3
+            eventrow.FilepPath = filePath
+            eventrow.isArchived = False
+            eventrow.Message = ''
+
+            tableStorage = tablestorage()
+            tableStorage.insertEventMetadata(eventrow.__dict__)
+
+            return func.HttpResponse("Func App successfully processed http request", status_code=200)
+
+        else:
+            logging.info(f"Ignoré : eventType={eventType}, className={className}")
+            return func.HttpResponse("Event not COMPLETE or ClassName Not Matched", status_code=204)
+
+    except Exception as e:
+        logging.error(f"Error processing request: {e}")
+        return func.HttpResponse(f"Error: {str(e)}\nPayload: {req.get_body()}", status_code=500)
